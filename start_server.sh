59a2d7810493419940aa443cc9283874e888098e
bossmania <bossmania412@gmail.com>
2026-02-01 11:20:25 -0500

    made a ban script that will ban a username from the list

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,5 @@
+#make it fail on error
+set -eu 
+
 # wrapper for starting the server on provided version
-python3 server_controller.py ~/server_versions/$1/TerrariaServer.bin.x86_64 -disableannouncementbox
+python3 server_controller.py ~/server_versions/$1/TerrariaServer.bin.x86_64 -disableannouncementbox -banlist ~/banlist.txt
