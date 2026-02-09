67860ff2fb1c5c8eff50a69146805e5bd74b720d
bossmania <bossmania412@gmail.com>
2026-02-08 23:15:47 -0500

    created the help msg

--- server_logic/discord_bot.py
+++ server_logic/discord_bot.py
@@ -4,13 +4,17 @@ import handle_commands
 import server_controller
 from discord.ext import commands
 
+#set the prefix used
+PREFIX = "!"
+
 def start_bot(TOKEN, proc):
     #set the discrod bot to have default permission and the ability to read messages
     intents = discord.Intents.default()
     intents.message_content = True
 
-    #create the bot client
-    bot = commands.Bot(command_prefix="!", intents=intents)
+    #create the bot client and disable the built in help command
+    bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+    bot.help_command = None
 
     #say when the bot is online
     @bot.event
@@ -61,6 +65,23 @@ def start_bot(TOKEN, proc):
         #settle the world's water
         handle_commands.settle(proc)
         await ctx.send(f"Sucessfully settled all of the water in the world!")
+
+    #help command
+    @bot.command()
+    async def help(ctx):
+        #create the embed with title, desc, and color
+        embed=discord.Embed(title="Help Commands", description="shows all of the help commands", color=0x2ec27e)
+
+        #set all of the commands
+        embed.add_field(name=f"{PREFIX}help", value="Shows the help message.")
+        embed.add_field(name=f"{PREFIX}kick <USERNAME>", value="Kicks a player from the server.")
+        embed.add_field(name=f"{PREFIX}ban <USERNAME>", value="Bans a player from the server.")
+        embed.add_field(name=f"{PREFIX}save", value="Save the world.")
+        embed.add_field(name=f"{PREFIX}exit", value="Save and exit the world.")
+        embed.add_field(name=f"{PREFIX}settle", value="Settle the moving water.")
+        
+        #send the embed
+        await ctx.send(embed=embed)
 
     #run the bot
     bot.run(TOKEN)
