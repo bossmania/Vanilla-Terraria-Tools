b393c5656454ea84c60129728e08d5cdd514d9a3
bossmania <62632773+bossmania@users.noreply.github.com>
2026-02-11 17:02:39 -0500

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,8 @@
 #make it fail on error
 set -eu 
 
+# put the user in the venv if they're not already
+source venv/bin/activate
+
 # wrapper for starting the server on provided version
 python3 server_logic/server_controller.py ~/server_versions/$1/TerrariaServer.bin.x86_64 -disableannouncementbox -banlist ~/admin/banlist.txt -config ~/admin/config.txt
