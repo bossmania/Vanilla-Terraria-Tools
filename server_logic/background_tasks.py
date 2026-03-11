adc3d3bb5ecfb3d28095ad808a472047648592fb
bossmania <bossmania412@gmail.com>
2026-03-11 18:17:41 -0400

    remove the ": " from the start of the string without affecting the actual ones

--- server_logic/background_tasks.py
+++ server_logic/background_tasks.py
@@ -38,7 +38,7 @@ def read_output(proc):
     for raw in proc.stdout:
         #clean up the line
         line = raw.rstrip("\n")
-        line = line.replace(": ", "")
+        line = line.removeprefix(": ")
 
         #control what happens based on the output
         output_controller.control_output(line, proc)
